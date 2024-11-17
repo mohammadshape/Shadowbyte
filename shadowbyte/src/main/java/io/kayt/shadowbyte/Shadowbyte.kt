2e74fd413e3658ef73c7124daa424af002bb1acd
Mohammad Fallah <mohammad@shape.dk>
2024-11-18 00:33:52 +0100

    Update annotation to use default values

--- shadowbyte/src/main/java/io/kayt/shadowbyte/Shadowbyte.kt
+++ shadowbyte/src/main/java/io/kayt/shadowbyte/Shadowbyte.kt
@@ -1,28 +1,37 @@
 package io.kayt.shadowbyte
 
+import java.lang.reflect.Proxy
+import kotlin.reflect.KClass
+
+inline fun <reified T> shadow(block: T.() -> Unit) {
+    val clazz = Class.forName("androidx.compose.material3.TabRowKt")
+    val method = clazz.getDeclaredMethod(
+        "access\$setScrollableTabRowMinimumTabWidth\$shadow",
+        Float::class.java
+    )
+    method.invoke(null, 0f)
+}
+
 class Shadowbyte {
 
     @Target(
         AnnotationTarget.FUNCTION,
         AnnotationTarget.PROPERTY
     )
-    @Retention(AnnotationRetention.SOURCE)
+    @Retention(AnnotationRetention.RUNTIME)
     @MustBeDocumented
-    @Suppress("unused")
-    annotation class Property
+    @Suppress("unused", "PropertyName")
+    annotation class Property(val name: String = "NOT_SPECIFIED_FIELD_NAME")
 
     @Target(
         AnnotationTarget.CLASS,
         AnnotationTarget.PROPERTY
     )
-    @Retention(AnnotationRetention.SOURCE)
+    @Retention(AnnotationRetention.RUNTIME)
     @MustBeDocumented
     @Suppress("unused")
-    annotation class Shadowed(val source: String)
-
-
-//    interface Shadowed {
-//        fun default(): Nothing =
-//            throw IllegalStateException("Make sure Shadowbyte-asm plugin is applied")
-//    }
+    annotation class Shadowed(
+        val qualifiedName: String = "NOT_SPECIFIED_SOURCE",
+        val source: KClass<*> = Unit::class
+    )
 }
